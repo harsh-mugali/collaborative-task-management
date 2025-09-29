d5bdf9bc2bcec48abf19600b33394fadde72570c
harsh <mugaliharsh@gmail.com>
2025-09-29 23:33:33 +0530

    added login page functionality

--- frontend/task-manager/src/pages/Auth/Login.jsx
+++ frontend/task-manager/src/pages/Auth/Login.jsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import { Link, useNavigate } from "react-router-dom"
 import Input from '../../components/Inputs/Input';
 import { validateEmail } from '../../utils/helper';
+import { API_PATHS } from '../../utils/apiPaths';
+import axiosInstance from '../../utils/axiosInstance';
 
 export const Login = () => {
   const [email, setEmail] = useState("");
@@ -16,57 +18,83 @@ export const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-  if (!validateEmail(email)) {
-    setError("Please enter a valid email address.");
-    return;
-  }
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
 
-  if (!password) {
-    setError("Please enter the password");
-    return;
-  }
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
 
-  setError("");
+    setError("");
 
-  //Login API Call
-};
-  return (
-    <AuthLayout>
-      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
-        <h3 className="text-xl front-semibold text-black">Welcome Back</h3>
-        <p className="test-xs text-slate-700 mt-[5px] mb-6">
-          Please enter your details to log in
-        </p>
-        <form onSubmit={handleLogin}>
-          <Input
-            value={email}
-            onChange={({ target }) => setEmail(target.value)}
-            label="Email Address"
-            placeholder="john@example.com"
-            type="text"
-          />
-          <Input
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-            label="Password"
-            placeholder="Min 8 Character"
-            type="password"
-          />
+    //Login API Call
+    try {
+      console.log(API_PATHS.AUTH.LOGIN);
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
 
-          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+      const { token, role } = response.data;
 
-          <button type="submit" className="btn-primary">
-            LOGIN
-          </button>
+      if (token) {
+        localStorage.setItem("token", token);
 
-          <p className="text-[13px] text-slate-800 mt-3">
-            Don't have an account?{" "}
-            <Link className="font-medium text-primary underline" to="/signup">
-              SignUp
-            </Link>
+        //Redirect based on role
+        if (role === "admin") {
+          navigate("/admin/dashboard");
+        } else {
+          navigate("/user/dashboard");
+        }
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
+    }
+    };
+    return (
+      <AuthLayout>
+        <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+          <h3 className="text-xl front-semibold text-black">Welcome Back</h3>
+          <p className="test-xs text-slate-700 mt-[5px] mb-6">
+            Please enter your details to log in
           </p>
-        </form>
-      </div>
-    </AuthLayout>
-  )
-}
+          <form onSubmit={handleLogin}>
+            <Input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="john@example.com"
+              type="text"
+            />
+            <Input
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              label="Password"
+              placeholder="Min 8 Character"
+              type="password"
+            />
+
+            {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+            <button type="submit" className="btn-primary">
+              LOGIN
+            </button>
+
+            <p className="text-[13px] text-slate-800 mt-3">
+              Don't have an account?{" "}
+              <Link className="font-medium text-primary underline" to="/signup">
+                SignUp
+              </Link>
+            </p>
+          </form>
+        </div>
+      </AuthLayout>
+    )
+  }
